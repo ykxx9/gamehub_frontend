e0c1c917171412be687d7a2c086cec7af1dc861c
MokshAjmeria <mokshajmeria@gmail.com>
2026-04-12 01:54:29 +0530

    testing html css & js

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
- function main(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        const newCard = document.createElement("div");
-        newCard.className = "card";
-        newCard.innerHTML = `
-        <img src="${arr[i].thumbnail}" />
-        <div class="product-info">
-            <h4> ${arr[i].title} </h4>
-            <p>Price: ${arr[i].price} </p>
-            <p>Rating: ${arr[i].rating} ⭐️</p>
-        </div>
-    `;
-        const root = document.getElementById("root");
-        root.appendChild(newCard);
-    }
- }
-
-
- async function getData() {
-    const URL = "https://dummyjson.com/products?select=title,price,rating,thumbnail";
-    const response = await fetch(URL);
-    const data = await response.json();
-    console.log(data);
- 
-    main(data.products);
- }
- 
- getData();
+function changeText() {
+    document.getElementById("title").innerText = "You clicked the button!";
+}
